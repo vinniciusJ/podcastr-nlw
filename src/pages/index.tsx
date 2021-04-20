a7f29350a4e3262203a21d2f4dc85b07756a8ddc
Vinicius <vinnicius.jimenez@gmail.com>
2021-04-20 16:11:55 -0300

    nlw: class 02 - maximum speed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
+const Home = ({ episodes }) => {
+    return (
+        <h1>Home</h1>
+    )
 }
+// Server side-rendering
+
+/*
+export const getServerSideProps = async () => {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return {
+        props: { episodes: data }
+    }
+}
+*/
+
+// Static server generation
+export const getStaticProps = async () => {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return {
+        props: { episodes: data },
+        revalidate: 60 * 60 * 8
+    }
+}
+
+export default Home
