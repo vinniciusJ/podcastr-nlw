0f22d8fc11eb437b1d6373644af6ae626fc4512a
Vinicius <vinnicius.jimenez@gmail.com>
2021-04-21 14:31:56 -0300

    add: axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
-const Home = ({ episodes }) => {
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+
+type Episode = {
+    id: string
+    title: string
+    members: string
+}
+
+type HomeProps = {
+    episodes: Episode[]
+}
+
+const Home = (props: HomeProps) => {
+    
     return (
         <h1>Home</h1>
     )
 }
-// Server side-rendering
-
-/*
-export const getServerSideProps = async () => {
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json()
-
-    return {
-        props: { episodes: data }
-    }
-}
-*/
 
 // Static server generation
-export const getStaticProps = async () => {
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await api.get('episodes', { params: {
+        _limit: 12,
+        _sort: 'published_at',
+        _order: 'desc'
+    } })
 
     return {
         props: { episodes: data },
